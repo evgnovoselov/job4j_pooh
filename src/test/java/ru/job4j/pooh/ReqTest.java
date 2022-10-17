3e317aeb3e26d72cee0c1fc03927b4b5a1ef81ee
Evgeny Novoselov <zkodru@gmail.com>
2022-10-17 13:54:28 +0300

    Пишу тест запроса очереди

--- src/test/java/ru/job4j/pooh/ReqTest.java
+++ src/test/java/ru/job4j/pooh/ReqTest.java
@@ -5,8 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReqTest {
-
     @Test
-    void of() {
+    void whenQueueModePostMethod() {
+        String ls = System.lineSeparator();
+        String content = "POST /queue/weather HTTP/1.1" + ls
+                + "Host: localhost:9000" + ls
+                + "User-Agent: curl/7.72.0" + ls
+                + "Accept: */*" + ls
+                + "Content-Length: 14" + ls
+                + "Content-Type: application/x-www-form-urlencoded" + ls
+                + "" + ls
+                + "temperature=18" + ls;
+        Req req = Req.of(content);
     }
 }
