89708bd3f22d5fdf1f58c423aaa6b755fbd09450
Evgeny Novoselov <zkodru@gmail.com>
2022-10-19 18:02:50 +0300

    Написал решение одного теста

--- src/main/java/ru/job4j/pooh/Req.java
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -1,5 +1,7 @@
 package ru.job4j.pooh;
 
+import java.util.Optional;
+
 public class Req {
     private final String httpRequestType;
     private final String poohMode;
@@ -14,7 +16,12 @@ public class Req {
     }
 
     public static Req of(String content) {
-        return new Req(null, null, null, null);
+        return new Req(
+                parseHttpRequestType(content),
+                parsePoohMode(content),
+                parseSourceName(content),
+                parseParam(content)
+        );
     }
 
     public String getHttpRequestType() {
@@ -30,6 +37,49 @@ public class Req {
     }
 
     public String getParam() {
+        return param;
+    }
+
+    private static String parseHttpRequestType(String content) {
+        String httpRequestType = null;
+        String type = content.substring(0, content.indexOf(" "));
+        if ("GET".equals(type)) {
+            httpRequestType = "GET";
+        }
+        if ("POST".equals(type)) {
+            httpRequestType = "POST";
+        }
+        return httpRequestType;
+    }
+
+    private static String parsePoohMode(String content) {
+        String poohMode = null;
+        String subs = content.substring(0, content.indexOf(System.lineSeparator()));
+        subs = subs.substring(subs.indexOf(" "), subs.lastIndexOf(" "));
+        String parseMode = subs.split("/", 3)[1];
+        if ("queue".equals(parseMode)) {
+            poohMode = "queue";
+        }
+        if ("topic".equals(parseMode)) {
+            poohMode = "topic";
+        }
+        return poohMode;
+    }
+
+    private static String parseSourceName(String content) {
+        String subs = content.substring(0, content.indexOf(System.lineSeparator()));
+        subs = subs.substring(subs.indexOf(" "), subs.lastIndexOf(" "));
+        return subs.split("/", 3)[2];
+    }
+
+    private static String parseParam(String content) {
+        String param = null;
+        Optional<String> contentLengthLine = content.lines().filter(s -> s.startsWith("Content-Length: ")).findFirst();
+        if (contentLengthLine.isPresent()) {
+            int contentLengthInt = Integer.parseInt(contentLengthLine.get()
+                    .substring(contentLengthLine.get().lastIndexOf(": ") + 2));
+            param = content.substring(content.length() - contentLengthInt - System.lineSeparator().length()).trim();
+        }
         return param;
     }
 }
