78732257253df140f2967100833c8a6034fc97d9
Evgeny Novoselov <zkodru@gmail.com>
2022-10-27 20:25:51 +0300

    Добавил тесты на сервисы

--- src/test/java/ru/job4j/pooh/QueueServiceTest.java
+++ src/test/java/ru/job4j/pooh/QueueServiceTest.java
@@ -2,11 +2,16 @@ package ru.job4j.pooh;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class QueueServiceTest {
 
     @Test
-    void process() {
+    void whenPostThenGetQueue() {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        queueService.process(new Req("POST", "queue", "weather", paramForPostMethod));
+        Resp result = queueService.process(new Req("GET", "queue", "weather", null));
+        assertThat(result.getText()).isEqualTo("temperature=18");
     }
 }
--- src/test/java/ru/job4j/pooh/TopicServiceTest.java
+++ src/test/java/ru/job4j/pooh/TopicServiceTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.pooh;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class TopicServiceTest {
 
     @Test
-    void process() {
+    void whenTopic() {
+        TopicService topicService = new TopicService();
+        String paramForPublisher = "temperature=18";
+        String paramForPublisher1 = "client407";
+        String paramForPublisher2 = "client6565";
+        topicService.process(new Req("GET", "topic", "weather", paramForPublisher1));
+        topicService.process(new Req("POST", "topic", "weather", paramForPublisher));
+        Resp result1 = topicService.process(
+                new Req("GET", "topic", "weather", paramForPublisher1)
+        );
+        Resp result2 = topicService.process(
+                new Req("GET", "topic", "weather", paramForPublisher2)
+        );
+        Assertions.assertThat(result1.getText()).isEqualTo("temperature=18");
+        Assertions.assertThat(result1.getText()).isEmpty();
     }
 }
