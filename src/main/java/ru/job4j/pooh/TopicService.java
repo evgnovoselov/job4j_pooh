f9ccf281ea0641b52b518df7baf928406845bcd8
Evgeny Novoselov <zkodru@gmail.com>
2022-10-27 20:49:38 +0300

    Исправил ошибку чекстайла

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,6 +1,6 @@
 package ru.job4j.pooh;
 
-public class TopicService implements Service{
+public class TopicService implements Service {
     @Override
     public Resp process(Req req) {
         return null;
