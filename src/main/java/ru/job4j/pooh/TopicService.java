8570311831b4e6f9306c6b951daa21ada4c36d73
Evgeny Novoselov <zkodru@gmail.com>
2022-10-28 14:32:31 +0300

    Добавил решение сервиса топика

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,8 +1,44 @@
 package ru.job4j.pooh;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
+
 public class TopicService implements Service {
+    private final ConcurrentMap<String, ConcurrentMap<String, ConcurrentLinkedQueue<String>>> topics = new ConcurrentHashMap<>();
+
     @Override
     public Resp process(Req req) {
-        return null;
+        Resp resp = null;
+        if ("GET".equals(req.getHttpRequestType())) {
+            resp = processGet(req);
+        }
+        if ("POST".equals(req.getHttpRequestType())) {
+            resp = processPost(req);
+        }
+        return resp;
+    }
+
+    private Resp processGet(Req req) {
+        topics.putIfAbsent(req.getSourceName(), new ConcurrentHashMap<>());
+        topics.get(req.getSourceName()).putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>());
+        String valueTopic = topics.get(req.getSourceName()).get(req.getParam()).poll();
+        String status = "200";
+        if (valueTopic == null) {
+            valueTopic = "";
+            status = "204";
+        }
+        return new Resp(valueTopic, status);
+    }
+
+    private Resp processPost(Req req) {
+        topics.computeIfPresent(req.getSourceName(), (topic, subs) -> {
+            subs.forEach((sub, queue) -> {
+                queue.add(req.getParam());
+            });
+            return subs;
+        });
+        return new Resp("", "200");
     }
 }
