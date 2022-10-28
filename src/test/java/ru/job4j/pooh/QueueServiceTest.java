adb129f11aed11e00eae27cf8ceb69bf865539f9
Evgeny Novoselov <zkodru@gmail.com>
2022-10-28 11:41:48 +0300

    Добавил еще тестов для проверки очереди

--- src/test/java/ru/job4j/pooh/QueueServiceTest.java
+++ src/test/java/ru/job4j/pooh/QueueServiceTest.java
@@ -14,4 +14,24 @@ class QueueServiceTest {
         Resp result = queueService.process(new Req("GET", "queue", "weather", null));
         assertThat(result.getText()).isEqualTo("temperature=18");
     }
+
+    @Test
+    void whenPostThenGetStatus200() {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        queueService.process(new Req("POST", "queue", "weather", paramForPostMethod));
+        Resp result = queueService.process(new Req("GET", "queue", "weather", null));
+        assertThat(result.getStatus()).isEqualTo("200");
+    }
+
+    @Test
+    void whenNoHaveDataThenStatus204() {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        queueService.process(new Req("POST", "queue", "weather", paramForPostMethod));
+        queueService.process(new Req("GET", "queue", "weather", null));
+        Resp result = queueService.process(new Req("GET", "queue", "weather", null));
+        assertThat(result.getText()).isEmpty();
+        assertThat(result.getStatus()).isEqualTo("204");
+    }
 }
