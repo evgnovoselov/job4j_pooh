d9dc36285fbe0e06590250093b85edd5eceaabd4
Evgeny Novoselov <zkodru@gmail.com>
2022-10-28 12:56:31 +0300

    Сделал реализацию сервиса очереди

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,8 +1,37 @@
 package ru.job4j.pooh;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
+
 public class QueueService implements Service {
+    private final ConcurrentMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
+
     @Override
     public Resp process(Req req) {
-        return null;
+        Resp resp = null;
+        if ("POST".equals(req.getHttpRequestType())) {
+            resp = processPost(req);
+        }
+        if ("GET".equals(req.getHttpRequestType())) {
+            resp = processGet(req);
+        }
+        return resp;
+    }
+
+    private Resp processGet(Req req) {
+        String valueQueue = queue.getOrDefault(req.getSourceName(), new ConcurrentLinkedQueue<>()).poll();
+        String status = "200";
+        if (valueQueue == null) {
+            valueQueue = "";
+            status = "204";
+        }
+        return new Resp(valueQueue, status);
+    }
+
+    private Resp processPost(Req req) {
+        queue.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
+        queue.get(req.getSourceName()).add(req.getParam());
+        return new Resp("", "200");
     }
 }
