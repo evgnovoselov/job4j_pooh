052fc26fb657c2eb7c2bd1b3a0e98fd4a8385f42
Evgeny Novoselov <zkodru@gmail.com>
2022-10-28 13:22:05 +0300

    Добавил еще один тест на двух подписчиков и исправил ошибку в подписчике

--- src/test/java/ru/job4j/pooh/TopicServiceTest.java
+++ src/test/java/ru/job4j/pooh/TopicServiceTest.java
@@ -10,17 +10,41 @@ class TopicServiceTest {
     void whenTopic() {
         TopicService topicService = new TopicService();
         String paramForPublisher = "temperature=18";
-        String paramForPublisher1 = "client407";
-        String paramForPublisher2 = "client6565";
-        topicService.process(new Req("GET", "topic", "weather", paramForPublisher1));
+        String paramForSubscriber1 = "client407";
+        String paramForSubscriber2 = "client6565";
+        topicService.process(new Req("GET", "topic", "weather", paramForSubscriber1));
         topicService.process(new Req("POST", "topic", "weather", paramForPublisher));
         Resp result1 = topicService.process(
-                new Req("GET", "topic", "weather", paramForPublisher1)
+                new Req("GET", "topic", "weather", paramForSubscriber1)
         );
         Resp result2 = topicService.process(
-                new Req("GET", "topic", "weather", paramForPublisher2)
+                new Req("GET", "topic", "weather", paramForSubscriber2)
         );
         Assertions.assertThat(result1.getText()).isEqualTo("temperature=18");
         Assertions.assertThat(result2.getText()).isEmpty();
+    }
+
+    @Test
+    void whenTopicTwoPublishersThenHaveTwoAnswer() {
+        TopicService topicService = new TopicService();
+        String paramForPublisher = "temperature=18";
+        String paramForSubscriber1 = "client407";
+        String paramForSubscriber2 = "client777";
+        String paramForSubscriber3 = "client6565";
+        topicService.process(new Req("GET", "topic", "weather", paramForSubscriber1));
+        topicService.process(new Req("GET", "topic", "weather", paramForSubscriber2));
+        topicService.process(new Req("POST", "topic", "weather", paramForPublisher));
+        Resp result1 = topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber1)
+        );
+        Resp result2 = topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber2)
+        );
+        Resp result3 = topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber3)
+        );
+        Assertions.assertThat(result1.getText()).isEqualTo("temperature=18");
+        Assertions.assertThat(result2.getText()).isEqualTo("temperature=18");
+        Assertions.assertThat(result3.getText()).isEmpty();
     }
 }
